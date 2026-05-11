61f7edec91ab173681939117f62a0804405ca3f9
camilo2210 <79025468+camilo2210@users.noreply.github.com>
2026-05-10 23:45:48 -0500

    one more again

---
+++ addons/controllers/portal.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import logging
+from odoo import http
+from odoo.http import request
+from odoo.addons.account_payment.controllers.portal import PortalAccount
+
+_logger = logging.getLogger(__name__)
+
+class PaymentCustomAmountPortal(PortalAccount):
+
+    @http.route()
+    def invoice_transaction(self, invoice_id, access_token, **kwargs):
+        # 1. Extraer valores del formulario
+        custom_amount = kwargs.get('custom_payment_amount')
+        custom_type = kwargs.get('custom_payment_type')
+
+        if custom_type == 'custom' and custom_amount:
+            try:
+                amount_float = float(custom_amount)
+                # GUARDAR EN SESIÓN: Esto es vital para que el modelo lo vea
+                request.session['custom_payment_amount'] = amount_float
+                
+                # FORZAR EN KWARGS: Odoo busca la llave 'amount'
+                kwargs['amount'] = amount_float
+                
+                _logger.info(">>> [PORTAL] Inyectando monto personalizado en sesión: %.2f", amount_float)
+            except (ValueError, TypeError):
+                _logger.error(">>> [PORTAL] Error al convertir monto: %s", custom_amount)
+
+        return super().invoice_transaction(invoice_id, access_token, **kwargs)
